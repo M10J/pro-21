a10a4bcf07b6af00600744b9751bc88d9fe7dc65
M10J <93811368+M10J@users.noreply.github.com>
2022-02-07 16:53:46 +0530

    Add files via upload

---
+++ Pro_c21_SA-main/sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+let ball;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+
+  ball=Bodies.circle(200,10,20,{restitution:0.95})
+
+  World.add(world,ball);
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+  ellipse(ball.position.x,ball.position.y,20);
+
+}
+
+function keyPressed(){
+  if(keyCode===UP_ARROW){
+
+    Matter.Body.applyForce(ball,ball.position,{x:0.05,y:-0.05})
+  }
+}
